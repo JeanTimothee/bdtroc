243922aa6614ba63549ac6876fc378d65b2b8f1d
Othmane Laghzaouni <o.laghzaouni@gmail.com>
2020-02-18 15:41:30 +0100

    review-text-only

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true, allow_blank: false
+  # validates :content, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
