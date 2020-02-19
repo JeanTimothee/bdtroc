87883cb81b6b14fc8b48f90f5c1494d021eb713d
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-02-19 10:46:38 +0100

    message for dynamic input banner

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('.banner-typed-text', {
-    strings: ["As Chuck Norris", "types with one finger."],
+    strings: ["As Chuck Norris", "type with one finger."],
     typeSpeed: 50,
     attr: 'placeholder',
     bindInputFocusEvents: true,
