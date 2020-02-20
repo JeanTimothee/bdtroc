57a1b25d1f5827045c523c2b5733dd748cda6512
renaud dor <renaud.dor@gmail.com>
2020-02-20 20:41:39 +0100

    create user-dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :bookings, only: [:index, :show]
+  get 'dashboard', to: 'dashboard#dashboard'
 end
